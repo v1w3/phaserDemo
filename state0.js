9585f26c68351ef367704925639560ea25266ad1
Viwe Mgoduka <mgodukaviwe@gmail.com>
2017-11-29 18:43:24 +0200

    Fixed some errors, just typos, got display working but switching states isn't working. it's console loggin but not swtiching. lemme check a look

--- state0.js
+++ state0.js
@@ -1,35 +1,40 @@
-var demo = {};
-
+var demo = {}, centerX = 1500 / 2, centerY = 1000 / 2;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('adam', 'assets/sprites/adam.png');
+    },
     create: function(){
-        game.stage.backgroundColor = '#35c511';    
+        game.stage.backgroundColor = '#35c511';  
+        console.log('state0');
         addChangeStateEventListeners();
         game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        var adam = game.add.sprite(centerX, centerY,  'adam');
+        adam.anchor.x = 0.5;
+        adam.anchor.y = 0.5;
     },
     update: function(){}
 
 };
 
-function changeState(1, stateNum) {
-    console.log(1);
+function changeState(i, stateNum) {
     game.state.start('state' + stateNum);
 }
 
 function addKeyCallback(key, fn, args){
-    game.input.keyboard.addKey(key).onDown.add(fn, null, null, args)
+    game.input.keyboard.addKey(key).onDown.add(fn, null, null, args);
 }
 
-function addChangeStateEventListeners() {
-      addKeyCallback(Phaser.keyboard.ZERO, changeState, 0);
-        addKeyCallback(Phaser.keyboard.ONE, changeState, 1);
-        addKeyCallback(Phaser.keyboard.TWO, changeState, 2);
-        addKeyCallback(Phaser.keyboard.THREE, changeState, 3);
-        addKeyCallback(Phaser.keyboard.FOUR, changeState, 4);
-        addKeyCallback(Phaser.keyboard.FIVE, changeState, 5);
-        addKeyCallback(Phaser.keyboard.SIX, changeState, 6);
-        addKeyCallback(Phaser.keyboard.SEVEN, changeState, 7);
-        addKeyCallback(Phaser.keyboard.EIGHT, changeState, 8);
-        addKeyCallback(Phaser.keyboard.NINE, changeState, 9); 
+function addChangeStateEventListeners(){
+      addKeyCallback(Phaser.Keyboard.ZERO, changeState, 0);
+        addKeyCallback(Phaser.Keyboard.ONE, changeState, 1);
+        addKeyCallback(Phaser.Keyboard.TWO, changeState, 2);
+        addKeyCallback(Phaser.Keyboard.THREE, changeState, 3);
+        addKeyCallback(Phaser.Keyboard.FOUR, changeState, 4);
+        addKeyCallback(Phaser.Keyboard.FIVE, changeState, 5);
+        addKeyCallback(Phaser.Keyboard.SIX, changeState, 6);
+        addKeyCallback(Phaser.Keyboard.SEVEN, changeState, 7);
+        addKeyCallback(Phaser.Keyboard.EIGHT, changeState, 8);
+        addKeyCallback(Phaser.Keyboard.NINE, changeState, 9); 
 }
