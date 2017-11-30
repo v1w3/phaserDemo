703a5e168d4f68c27dcb2e5e7b6c02877b666f4b
Viwe Mgoduka <mgodukaviwe@gmail.com>
2017-12-01 00:52:19 +0200

    state_2 done - Shootin Example

--- state3.js
+++ state3.js
@@ -1,10 +1,16 @@
 demo.state3 = function(){};
 demo.state3.prototype = {
-    preload: function(){},
+    preload: function(){
+        
+        
+
+    },
     create: function(){
         game.stage.backgroundColor = '#ff9900';
         
         addChangeStateEventListeners();
+        
+        
     },
     update: function(){}
 
