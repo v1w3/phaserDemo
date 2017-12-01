58fb9cea16f1c33fe2e47e478ec9a9d80fe7cbbb
Viwe Mgoduka <mgodukaviwe@gmail.com>
2017-12-01 08:44:26 +0200

    State_5 = Velocity and Accelation

--- state5.js
+++ state5.js
@@ -1,11 +1,46 @@
+var accel = 400, platform, plaformGroup;
+
 demo.state5 = function(){};
 demo.state5.prototype = {
-    preload: function(){},
+    preload: function(){
+        game.load.image('platform', 'assets/sprites/platform.png');
+    },
     create: function(){
         game.stage.backgroundColor = '#ff0000';
         
         addChangeStateEventListeners();
+        
+        adam = game.add.sprite(centerX, 500, 'adam');
+        platform = game.add.sprite(0, 800, 'platform');
+        plaformGroup = game.add.group();
+        plaformGroup.create(650, 400, 'platform');
+        plaformGroup.create(1050, 600, 'platform');
+
+        
+        game.physics.enable([adam, platform, plaformGroup]);
+        
+        adam.body.gravity.y = 500;
+        adam.body.bounce.y = 0.3;
+        adam.body.drag = 400;
+        adam.body.collideWorldBounds = true;
+        
+        platform.body.immovable = true;
+        
+        plaformGroup.setAll('body.immovable', true);
     },
-    update: function(){}
+    update: function(){
+        game.physics.arcade.collide(adam, [platform, plaformGroup]);
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            adam.body.acceleration.x = -accel;
+        } else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            adam.body.acceleration.x = accel;
+        } else {
+            adam.body.acceleration.x = 0;
+        }
+        
+        if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+            adam.body.velocity.y = -300;
+        }
+    }
 
 }
